486e1850094e2d62bc9c568fc576a017a43604de
cksar <cesarhsouzasoad@gmail.com>
2020-08-27 18:40:07 -0300

    Alertify e varificações da cabeça adicionada

--- js/headController.js
+++ js/headController.js
@@ -1,27 +1,55 @@
-function headInclinationUp(){
-    APIROBO.put("/api/head/inclination/UP").then(function (response) {
-        console.log(response)
-        reload()
-    })
+function headInclination(position) {
+    APIROBO.put("/api/head/inclination/" + position)
+        .then(function (response) {
+            if (response.data == document.getElementById('head-inclination').innerText) {
+                alertify.warning('Impossível inclinar mais!!!')
+            } else {
+                reload()
+                alertify.success("Inclinado com sucesso")
+            }
+        })
+        .catch(function (error) {
+            // Erro
+            alertify.error('Não possível atualizar a posição');
+        });
 }
 
-function headInclinationDown(){
-    APIROBO.put("/api/head/inclination/DOWN").then(function (response) {
-        console.log(response)
-        reload()
-    })
+function headRotationLeft() {
+    APIROBO.put("/api/head/rotation/1")
+        .then(function (response) {
+            if (document.getElementById('head-inclination').innerText != "DOWN") {
+                if (response.data == document.getElementById('head-rotation').innerText) {
+                    alertify.warning('Impossível rotacionar mais!!!')
+                } else {
+                    reload()
+                    alertify.success("Rotacionado com sucesso")
+                }
+            } else {
+                alertify.warning('Impossível rotacionar com a cabeça para baixo!!!')
+            }
+        })
+        .catch(function (error) {
+            // Erro
+            alertify.error('Não possível atualizar a posição');
+        });
 }
 
-function headRotationLeft(){
-    APIROBO.put("/api/head/rotation/1").then(function (response) {
-        console.log(response)
-        reload()
-    })
-}
-
-function headRotationRight(){
-    APIROBO.put("/api/head/rotation/-1").then(function (response) {
-        console.log(response)
-        reload()
-    })
+function headRotationRight() {
+    APIROBO.put("/api/head/rotation/-1")
+        .then(function (response) {
+            if (document.getElementById('head-inclination').innerText != "DOWN") {
+                if (response.data == document.getElementById('head-rotation').innerText) {
+                    alertify.warning('Impossível rotacionar mais!!!')
+                } else {
+                    reload()
+                    alertify.success("Rotacionado com sucesso")
+                }
+            } else {
+                alertify.warning('Impossível rotacionar com a cabeça para baixo!!!')
+            }
+        })
+        .catch(function (error) {
+            // Erro
+            alertify.error('Não possível atualizar a posição');
+        });
 }
