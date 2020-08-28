3ae27c6925faea92fb04db1ad23a08bcddd822c4
cksar <cesarhsouzasoad@gmail.com>
2020-08-27 23:45:10 -0300

    Bootstrap adicionado

--- js/status.js
+++ js/status.js
@@ -3,28 +3,27 @@ showStatus()
 function showStatus(){
     APIROBO.get("/api/head/inclination").then(function (response) {
         let headInclination = document.getElementById('head-inclination')
-        console.log(response)
-        headInclination.innerHTML = "<p>"+response.data+"</p>"
+        headInclination.innerHTML = response.data
     })
     APIROBO.get("/api/head/rotation").then(function (response) {
         let headRotation = document.getElementById('head-rotation')
-        headRotation.innerHTML = "<p>"+response.data+"</p>"
+        headRotation.innerHTML = response.data
     })
     APIROBO.get("/api/arms/elbow/left").then(function (response) {
         let elbowLeft = document.getElementById('elbow-left')
-        elbowLeft.innerHTML = "<p>"+response.data+"</p>"
+        elbowLeft.innerHTML = response.data
     })
     APIROBO.get("/api/arms/wrist/left").then(function (response) {
         let wristLeft = document.getElementById('wrist-left')
-        wristLeft.innerHTML = "<p>"+response.data+"</p>"
+        wristLeft.innerHTML = response.data
     })
     APIROBO.get("/api/arms/elbow/right").then(function (response) {
         let elbowRight = document.getElementById('elbow-right')
-        elbowRight.innerHTML = "<p>"+response.data+"</p>"
+        elbowRight.innerHTML = response.data
     })
     APIROBO.get("/api/arms/wrist/right").then(function (response) {
         let wristRight = document.getElementById('wrist-right')
-        wristRight.innerHTML = "<p>"+response.data+"</p>"
+        wristRight.innerHTML = response.data
     })
 }
 
